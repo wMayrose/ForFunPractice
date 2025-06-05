b78ad64dbbf467e8f80865c9f06207c422285b7c
Willa Mayrose <francesmay775@gmail.com>
2025-06-05 10:33:29 -0500

    Initial Commit

---
+++ ForFunPractice/ForFunPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForFunPracticeApp.swift
+//  ForFunPractice
+//
+//  Created by Scholar on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ForFunPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
