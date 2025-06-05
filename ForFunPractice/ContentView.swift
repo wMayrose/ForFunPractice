448d378ef3afd8d837fa912f9e12630eb24b4978
Willa Mayrose <francesmay775@gmail.com>
2025-06-05 11:55:18 -0500

    background color added

--- ForFunPractice/ContentView.swift
+++ ForFunPractice/ContentView.swift
@@ -9,19 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image("me")
-            .resizable()                  // Makes the image resizable
-            .frame(width: 350, height: 350) // Sets the size (make smaller or bigger)
-            .aspectRatio(contentMode: .fit) // Keeps the aspect ratio so it doesn’t get stretched
-            .cornerRadius(170)
-            Spacer()
-            Text("My Practice")
-                .font(.largeTitle)
-                .foregroundColor(.black)
-                .fontWeight(.bold)
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+            VStack {
+                Image("me")
+                    .resizable()                  // Makes the image resizable
+                    .frame(width: 350, height: 350) // Sets the size (make smaller or bigger)
+                    .aspectRatio(contentMode: .fit) // Keeps the aspect ratio so it doesn’t get stretched
+                    .cornerRadius(170)
+                Spacer()
+                Text("My Practice")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                
+            } // end VStack
             
-        } // end VStack
+        } // ZStack
     } // end body
 } // end struct
 
